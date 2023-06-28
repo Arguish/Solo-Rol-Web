d151d892c296563baf458f4a1bd8e69f9532e26a
Arguish <jhergon8@gmail.com>
2023-06-28 13:04:10 +0100

    hardcoded

--- api/db/dbIndex.js
+++ api/db/dbIndex.js
@@ -4,7 +4,9 @@ let db;
 
 const checkConnection = async () => {
   if (!db) {
-    db = await mongoose.connect(process.env.MONGURI);
+    db = await mongoose.connect(
+      "mongodb+srv://Admin:Admin@cluster0.c09jwda.mongodb.net/todoteams?retryWrites=true&w=majority"
+    );
   }
   console.log(
     `Conectet to "${db.connection.name}" on port ${db.connection.port}`
